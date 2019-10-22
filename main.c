9eb688c595f3ff02e6143034898e53b5de3aa617
Christoph Berger <c.berger@tum.de>
2019-10-22 16:52:10 +0200

    cleanup

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  tutorial 1
+//
+//  Created by Christoph Berger on 06.10.19.
+//  Copyright © 2019 Christoph Berger. All rights reserved.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
